a1eb53d78cba988b8a17b637ba6d2d395bcd7331
anwer25 <azern72@gmail.com>
2022-05-22 18:21:19 +0100

    starter

---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Home } from "./pages";
+
+
+/**
+ * @author anwer nouri
+ * @license MIT
+ * @description  REACT main App component
+ * @returns jsx element
+ */
+
+const App: React.FC = (): JSX.Element => <Home />;
+
+export default App;
