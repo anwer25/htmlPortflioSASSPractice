b72534a664e23eef27171c77c44b6795523fbb3d
anwer25 <azern72@gmail.com>
2022-05-26 23:38:36 +0100

    add text fonts

--- app/src/components/sections/intro/index.tsx
+++ app/src/components/sections/intro/index.tsx
@@ -7,6 +7,16 @@ import React from "react";
  * @returns JSX.Element
  */
 
-const Intro: React.FC = (): JSX.Element => <section id="intro">index</section>;
+const Intro: React.FC = (): JSX.Element => (
+	<section id="intro">
+		<p className="name">
+			Hi, my name is <span>anwer nouri.</span>
+		</p>
+		<h2>I create the front end of websites</h2>
+		<h1>I&apos;m a full stack developer</h1>
+		<p>specializing in React, SASS, Rest Api, GraphQl</p>
+		<p> Currently, I&apos;m working as A freelancer</p>
+	</section>
+);
 
 export default Intro;
