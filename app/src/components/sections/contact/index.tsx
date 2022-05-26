b458dd7d69a0d9aaa8e4ce612890aae297f59bc0
anwer25 <azern72@gmail.com>
2022-05-27 00:12:37 +0100

    start on contact section

---
+++ app/src/components/sections/contact/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/**
+ * @description Intro section component
+ * @author anwer nouri
+ * @props null
+ * @returns JSX.Element
+ */
+
+const Contact: React.FC = (): JSX.Element => <div>index</div>;
+
+export default Contact;
--- app/src/components/sections/index.ts
+++ app/src/components/sections/index.ts
@@ -2,3 +2,4 @@
  * @description export Intro component
  */
 export {default as Intro} from './intro'
+export {default as Contact} from './contact'
